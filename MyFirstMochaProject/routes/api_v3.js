aec0258f0ba5b9b057527d84941b2617ae390e9c
Felix Boons <felixboons@hotmail.com>
2017-05-16 19:44:37 +0200

    all-country-endpoints-added

--- MyFirstMochaProject/routes/api_v3.js
+++ MyFirstMochaProject/routes/api_v3.js
@@ -8,7 +8,6 @@ var router = express.Router();
 var pool = require('../db/db_connector');
 
 
-
 // router.get('/cities', function (req, res) {
 //     var query_str;
 //     if (citi) {
@@ -17,8 +16,66 @@ var pool = require('../db/db_connector');
 //         query_str = "SELECT * FROM actor_info";
 //     }
 
+// pool.getConnection(function (err, connection) {
+//     connection.query(query_str, function (err, rows, fields) {
+//         connection.release();
+//         if (err) {
+//             throw err
+//         }
+//         res.status(200).json(rows);
+//     });
+// });
+
+//select countries, filtered on continent an set an optional limit to amount of countries shown.
+router.get('/countries/search', function(req, res) {
+
+    var continent = req.query.continent || '';
+    var limit = req.query.limit || '';
+    var query = '';
+    console.log(continent + ' / ' + limit + '----------------');
+
+    if (continent && limit) {
+        query = "SELECT * FROM country where continent = '" + continent + "' LIMIT " + limit + ";";
+        console.log(query);
+    } else if (continent) {
+        query = "SELECT * FROM country where continent = '" + continent + "';";
+        console.log(query);
+    } else if (limit) {
+        query = "SELECT * FROM country LIMIT + '" + limit + "';";
+        console.log(query);
+    }
+
+    if (query !== '') {
+        pool.getConnection(function (err, connection) {
+            connection.query(query, function (err, rows) {
+                connection.release();
+                if (err) {
+                    throw err;
+                }
+                res.status(200).json(rows);
+            })
+        })
+    } else {
+        console.log('Search query is empty.');
+        res.send('Search query is empty. Example query: /api/v3/countries/search?continent=Europe&limit=10 .');
+    }
+});
+
+
+//select all countries or one country by name
+router.get('/countries/:name?', function (req, res) {
+
+    var countryName = req.params.name || '';
+    var query = '';
+
+    if (countryName) {
+        query = 'SELECT * FROM country WHERE name = "' + countryName + '";'
+    } else {
+        query = 'SELECT * FROM country';
+    }
+
     pool.getConnection(function (err, connection) {
-        connection.query(query_str, function (err, rows, fields) {
+        connection.query(query, function (err, rows, fields) {
             connection.release();
             if (err) {
                 throw err
@@ -28,31 +85,75 @@ var pool = require('../db/db_connector');
     });
 });
 
-// router.get('/recipes', function(req, res) {
-//     res.status(200);
-//
-//     var ingredient = req.query.ingredient || '';
-//
-//     var recipe = recipes.filter(function(r) {
-//         return (r.ingredient.toLowerCase() === ingredient.toLowerCase());
-//     });
-//
-//     res.json(recipe);
-// });
+//add country
+router.post('/countries/new/:code', function (req, res) {
 
-router.get('/recipes', function (req, res) {
-    res.status(200);
-    res.json(recipes);
+    var landCode = req.params.code;
 
+    var query = 'INSERT INTO country VALUES("' + landCode + '", "Gekkelandje", "Gekke continentje",' +
+        '"Gekke regiotje", "Surface areatje", 2016, 9001, 105.2, 0.00, 0.00,' +
+        ' "Lokale naam", "Reepoeblique", "Felix Boons", 130, "XX")';
+
+    pool.getConnection(function (err, connection) {
+        connection.query(query, function (err, rows, fields) {
+            connection.release();
+            if (err) {
+                throw err;
+            }
+            console.log('New country created with primary key ' + landCode);
+            res.status(200).json(rows);
+        })
+    });
 });
 
-router.get('/recipes/:number', function (req, res) {
-    res.status(200);
+//update country
+//WERKT NOG NIET. RUNT WEL ZONDER ERRORS, MAAR UPDATE NOG GEEN DATA.
+router.post('/countries/update/:code/:name', function (req, res) {
 
-    var number = req.params.number || '';
-    var recipe = recipes[number - 1];
+    var landCode = req.params.code;
+    var name = req.params.name;
 
-    res.json(recipe);
+    var query = 'UPDATE country SET name = "' + name + '" WHERE country.code = "' + landCode + '";';
+
+    pool.getConnection(function (err, connection) {
+        connection.query(query, function (err, rows, fields) {
+            if (err) {
+                throw err;
+            }
+            res.status(200).json(rows);
+            if (rows.rowsAffected === 0) {
+                console.log("No rows were affected!");
+            } else {
+                console.log('Country with code ' + landCode + ' has been updated to name "' + name + '".');
+            }
+        });
+    });
+});
+
+//delete country
+//WERKT NOG NIET. RUNT WEL ZONDER ERRORS, MAAR DELETE NOG GEEN DATA.
+router.delete('/countries/delete/:code', function (req, res) {
+
+    var code = req.params.code;
+
+    var query = 'DELETE FROM `country` WHERE `country`.`code` = "' + code + '";';
+
+    pool.getConnection(function (err, connection) {
+        connection.query(query, function (err, rows, fields) {
+            if (err.ER_ROW_IS_REFERENCED) {
+                res.send('Foreign key constraint. Cannot delete this row');
+            }
+            if (err) {
+                throw err;
+            }
+            res.status(200).json(rows);
+            if (rows.affectedRows === 0) {
+                console.log('No rows were affected!');
+            } else {
+                console.log('Country with code ' + code + ' has been deleted from the database.');
+            }
+        });
+    });
 });
 
 router.get('*', function (request, response) {
